c8b1a0e0c7610eacc126043fdda37d1744768b88
bethyd <bethyd123@gmail.com>
2017-12-20 18:28:02 -0500

    begin ops

---
+++ ops.py
@@ -0,0 +1,12 @@
+import math
+import numpy as np 
+import tensorflow as tf 
+from tensorflow.python.framework import ops
+from utils import *
+
+# try:
+#     image_summary = tf.image_summary
+#     scalar_summary = tf.scalar_summary
+#     histogram_summary = tf.histogram_summary
+#     merge_summary = tf.merge_summary
+#     SummaryWriter = tf.train.SummaryWriter
