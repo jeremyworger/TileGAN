ca69c79cdec1fd9d1a038aa375f86d2bf09a7ec3
bethyd <bethyd123@gmail.com>
2018-01-05 21:01:49 -0500

    adding ops and model

--- main.py
+++ main.py
@@ -19,26 +19,32 @@ import model
 from model import weights
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataRoot', required=True, help='path to dataset')
-parser.add_argument('--workers', type=int, default=2, help='number of data loading workers')
-parser.add_argument('--batchSize', type=int, default=8, help='input batch size')
-parser.add_argument('--imageSize', type=int, default=224, help='the height / width of the input image to network')
+parser.add_argument('--dataroot', required=True, help='path to dataset')
+parser.add_argument('--workers', type=int, help='number of data loading workers', default=2)
+parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
+parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
 parser.add_argument('--nz', type=int, default=100, help='size of the latent z vector')
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
 parser.add_argument('--niter', type=int, default=25, help='number of epochs to train for')
-parser.add_argument('--lr', type=float, default=0.0002, help='learning rate, default=0.0002')
+parser.add_argument('--lrD', type=float, default=0.00005, help='learning rate for Critic, default=0.0002')
+parser.add_argument('--lrG', type=float, default=0.00005, help='learning rate for Generator, default=0.0002')
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
 parser.add_argument('--cuda'  , action='store_true', help='enables cuda')
 parser.add_argument('--ngpu'  , type=int, default=1, help='number of GPUs to use')
 parser.add_argument('--netG', default='', help="path to netG (to continue training)")
 parser.add_argument('--netD', default='', help="path to netD (to continue training)")
+parser.add_argument('--clamp_lower', type=float, default=-0.01)
+parser.add_argument('--clamp_upper', type=float, default=0.01)
+parser.add_argument('--Diters', type=int, default=5, help='number of D iters per each G iter')
+parser.add_argument('--grad_bound', type=float, default=1e10, help='Keep training the disc until the norm of its gradient is below this')
+parser.add_argument('--n_extra_layers', type=int, default=0, help='Number of extra layers on gen and disc')
+parser.add_argument('--experiment', default=None, help='Where to store samples and models')
+parser.add_argument('--adam', action='store_true', help='Whether to use adam (default is rmsprop)')
 parser.add_argument('--outDir', default='.', help='folder to output images and model checkpoints')
-# parser.add_argument('--model', type=int, default=1, help='1 for dcgan, 2 for illustrationGAN-like-GAN')
-parser.add_argument('--d_labelSmooth', type=float, default=0, help='for D, use soft label "1-labelSmooth" for real samples')
 parser.add_argument('--n_extra_layers_d', type=int, default=0, help='number of extra conv layers in D')
 parser.add_argument('--n_extra_layers_g', type=int, default=1, help='number of extra conv layers in G')
-parser.add_argument('--binary', action='store_true', help='z from bernoulli distribution, with prob=0.5')
+
 
 opt = parser.parse_args()
 print(opt)
@@ -67,7 +73,7 @@ n_extra_g = opt.n_extra_layers_g
 img_size = opt.imageSize
 
 dataset = dset.ImageFolder(
-    root=opt.dataRoot,
+    root=opt.dataroot,
     transform=transforms.Compose([
             transforms.Scale(opt.imageSize),
             transforms.CenterCrop(opt.imageSize),
@@ -79,8 +85,8 @@ dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
                                          shuffle=True, num_workers=opt.workers)
 
 # load model
-netG = model.NetG1(ngpu, nz, nc, ngf, n_extra_g)
-netD = model.NetD1(ngpu, nz, nc, ndf, n_extra_d)
+netG = model.NetG(opt.imageSize, nz, nc, ngf, ngpu, n_extra_g)
+netD = model.NetD(opt.imageSize, nz, nc, ndf, ngpu, n_extra_d)
 
 netG.apply(weights)
 if opt.netG != '':
@@ -118,8 +124,8 @@ noise = Variable(noise)
 fixed_noise = Variable(fixed_noise)
 
 # setup optimizer
-optimizerD = optim.Adam(netD.parameters(), lr = opt.lr, betas = (opt.beta1, 0.999))
-optimizerG = optim.Adam(netG.parameters(), lr = opt.lr, betas = (opt.beta1, 0.999))
+optimizerD = optim.Adam(netD.parameters(), lr = opt.lrD, betas = (opt.beta1, 0.999))
+optimizerG = optim.Adam(netG.parameters(), lr = opt.lrG, betas = (opt.beta1, 0.999))
 
 # begin training for n epochs
 for epoch in range(opt.niter):
--- model.py
+++ model.py
@@ -10,144 +10,93 @@ def weights(m):
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
 
-# DCGAN model, fully convolutional architecture
-class NetG1(nn.Module):
-    def __init__(self, ngpu, nz, nc , ngf, n_extra_layers_g):
-        super(NetG1, self).__init__()
+class NetG(nn.Module):
+    def __init__(self, image_size, nz, nc, ngf, ngpu, n_extra_layers=0):
+        super(NetG, self).__init__()
         self.ngpu = ngpu
-        main = nn.Sequential(
-            # input is Z, going into a convolution
-            nn.ConvTranspose2d(nz, ngf * 32, 4, 1, 0, bias=False),
-            nn.BatchNorm2d(ngf * 32),
-            nn.ReLU(True),
-            # state size: (ngf*32) x 4 x 4
-            nn.ConvTranspose2d(ngf * 32, ngf * 16, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 16),
-            nn.ReLU(True),
-            # state size: (ngf*16) x 8 x 8
-            nn.ConvTranspose2d(ngf * 16, ngf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 8),
-            nn.ReLU(True),
-            # state size: (ngf*8) x 16 x 16
-            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 4),
-            nn.ReLU(True),
-            # state size. (ngf*4) x 32 x 32
-            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf * 2),
-            nn.ReLU(True),
-            # state size. (ngf*2) x 64 x 64
-            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ngf),
-            nn.ReLU(True),
-            # state size. (ngf) x 128 x 128
-            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
-            nn.Tanh()
-            # state size: (nc) x 256 x 256
+        assert image_size % 16 == 0, "image_size has to be a multiple of 16"
 
+        cngf, tisize = ngf//2, 4
+        while tisize != image_size:
+            cngf = cngf * 2
+            tisize = tisize * 2
+
+        main = nn.Sequential(
+            # input is Z going into a convolution
+            nn.ConvTranspose2d(nz, ngf, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(cngf),
+            nn.ReLU(True),
         )
+        i, csize, cndf = 3, 4, cngf
 
-        # # Extra layers
-        # for t in range(n_extra_layers_g):
-        #     main.add_module('extra-layers-{0}.{1}.conv'.format(t, ngf),
-        #                     nn.Conv2d(ngf, ngf, 3, 1, 1, bias=False))
-        #     main.add_module('extra-layers-{0}.{1}.batchnorm'.format(t, ngf),
-        #                     nn.BatchNorm2d(ngf))
-        #     main.add_module('extra-layers-{0}.{1}.relu'.format(t, ngf),
-        #                     nn.LeakyReLU(0.2, inplace=True))
+        while csize < image_size//2:
+            main.add_module(str(i), nn.ConvTranspose2d(cngf, cngf//2, 4, 2, 1, bias=False))
+            main.add_module(str(i+1), nn.BatchNorm2d(cngf//2))
+            main.add_module(str(i+2), nn.ReLU(True))
 
-        # main.add_module('final_layer.deconv', 
-        # 	             nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False)) # 5,3,1 for 96x96
-        # main.add_module('final_layer.tanh', 
-        # 	             nn.Tanh())
-        #     # state size. (nc) x 96 x 96
+            i += 3
+            cngf = cngf // 2
+            csize = csize * 2
 
+        # Extra layers
+        for j in range(n_extra_layers):
+            main.add_module(str(i), nn.Conv2d(cngf, cngf, 3, 1, 1, bias=False))
+            main.add_module(str(i+1), nn.BatchNorm2d(cngf))
+            main.add_module(str(i+2), nn.ReLU(True))
+            i += 3      
+        
+        # state size: K x 4 x 4
+        main.add_module(str(i), nn.ConvTranspose2d(cngf, nc, 4, 2, 1, bias=False))
+        main.add_module(str(i+1), nn.Tanh())
         self.main = main
-
-
+    
     def forward(self, input):
+        gpu_ids = None
         if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
-            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
-        else:
-            output = self.main(input)
-        return output
+            gpu_ids = range(self.ngpu)
+        return nn.parallel.data_parallel(self.main, input, gpu_ids)
 
-class NetD1(nn.Module):
-    def __init__(self, ngpu, nz, nc, ndf,  n_extra_layers_d):
-        super(NetD1, self).__init__()
+    
+
+class NetD(nn.Module):
+    def __init__(self, image_size, nz, nc, ndf, ngpu, n_extra_layers=0):
+        super(NetD, self).__init__()
         self.ngpu = ngpu
-        main = nn.Sequential(
-            # # input is (nc) x 96 x 96
-            # nn.Conv2d(nc, ndf, 4, 2, 1, bias=False), # 5,3,1 for 96x96
-            # nn.LeakyReLU(0.2, inplace=True),
-            # # state size. (ndf) x 32 x 32
-            # nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-            # nn.BatchNorm2d(ndf * 2),
-            # nn.LeakyReLU(0.2, inplace=True),
-            # # state size. (ndf*2) x 16 x 16
-            # nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-            # nn.BatchNorm2d(ndf * 4),
-            # nn.LeakyReLU(0.2, inplace=True),
-            # # state size. (ndf*4) x 8 x 8
-            # nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-            # nn.BatchNorm2d(ndf * 8),
-            # nn.LeakyReLU(0.2, inplace=True),
-            # # state size. (ndf*8) x 4 x 4
+        assert image_size % 16 == 0, "image_size has to be a multiple of 16"
 
-            # input is (nc) x 256 x 256
+        main = nn.Sequential(
+            # input is nc x image_size x image_size
             nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size: (ndf) x 128 x 128
-            nn.Conv2d(ndf, ndf * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 2),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size: (ndf*2) x 96 x 96
-            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 4),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size: (ndf*4) x 96 x 96
-            nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 4),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size: (ndf*8) x 64 x 64
-            nn.Conv2d(ndf * 4, ndf * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 8),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size: (ndf*16) x 32 x 32
-            nn.Conv2d(ndf * 8, ndf * 16, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 16),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size: (ndf*32) x 16 x 16
-            nn.Conv2d(ndf * 16, ndf * 32, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 32),
-            nn.LeakyReLU(0.2, inplace=True),
-            # state size: (ndf*64) x 8 x 8
-            nn.Conv2d(ndf * 32, ndf * 64, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(ndf * 64),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(ndf * 64, 1 , 4, 2, 1, bias=False),
-            nn.Sigmoid()
-            
+            nn.LeakyReLU(0.2, inplace=True)
         )
+        i, csize, cndf = 2, image_size / 2, ndf
 
-        # # Extra layers
-        # for t in range(n_extra_layers_d):
-        #     main.add_module('extra-layers-{0}.{1}.conv'.format(t, ndf * 8),
-        #                     nn.Conv2d(ndf * 8, ndf * 8, 3, 1, 1, bias=False))
-        #     main.add_module('extra-layers-{0}.{1}.batchnorm'.format(t, ndf * 8),
-        #                     nn.BatchNorm2d(ndf * 8))
-        #     main.add_module('extra-layers-{0}.{1}.relu'.format(t, ndf * 8),
-        #                     nn.LeakyReLU(0.2, inplace=True))
+        # Extra layers
+        for j in range(n_extra_layers):
+            main.add_module(str(i), nn.Conv2d(cndf, cndf, 3, 1, 1, bias=False))
+            main.add_module(str(i+1), nn.BatchNorm2d(cndf))
+            main.add_module(str(i+2), nn.LeakyReLU(0.2, inplace=True))
+            i += 3
+        
+        while csize > 4:
+            in_feature = cndf
+            out_feature = cndf * 2
+            main.add_module(str(i), nn.Conv2d(in_feature, out_feature, 4, 2, 1, bias=False))
+            main.add_module(str(i+1), nn.BatchNorm2d(out_feature))
+            main.add_module(str(i+2), nn.LeakyReLU(0.2, inplace=True))
 
-
-        # main.add_module('final_layers.conv', nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=False))
-        # main.add_module('final_layers.sigmoid', nn.Sigmoid())
-        # state size. 1 x 1 x 1
+            i += 3
+            cndf = cndf * 2
+            csize = csize / 2
+        
+        # state size: K x 4 x 4
+        main.add_module(str(i), nn.Conv2d(cndf, 1, 4, 1, 0, bias=False))
         self.main = main
-
+    
     def forward(self, input):
+        gpu_ids = None
         if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
-            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
-        else:
-            output = self.main(input)
-        return output.view(-1, 1).squeeze(1)
+            gpu_ids = range(self.ngpu)
+        output = nn.parallel.data_parallel(self.main, input, gpu_ids)
+        output = output.mean(0)
+        return output.view(1)
